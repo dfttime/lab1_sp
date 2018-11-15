5baaf734630d8c9fa28971b155b4ec78e9abbad9
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    some changes

--- calculator.cpp
+++ calculator.cpp
@@ -2,10 +2,15 @@
 
 int Calculator::Add (double a, double b)
 {
-	return a + b + 0.5;
+	return (int)(a + b + 0.5);
 }
 
 int Calculator::Sub (double a, double b)
 {
-    return Add (a, -b);
+	return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+	return (int)(a * b + 0.5);
 }
